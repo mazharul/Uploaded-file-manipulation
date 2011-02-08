422705bb43d1c34d6e3fde37b42c750e8f647598
Mazharul <mazharul2007@gmail.com>
2011-02-08 12:28:00 +0800

    better docs

--- uploader.php
+++ uploader.php
@@ -1,8 +1,9 @@
 <?php
-/*--------------------------------------------------------
-* @ example php page to implement MazUploadClass.php
-* @author: Mazharul Anwar(mazharul2007@gmail.com)
-*------------------------------------------------------------*/
+/**
+* example php page to implement MazUploadClass.php
+* @author Mazharul Anwar(mazharul2007@gmail.com)
+* @package applies_to_uploadClass
+*/
 
 include_once('MazUploadClass.php');
 $uploadpdf = new MazUploadClass(); //creating new object class
@@ -10,10 +11,10 @@ $uploadpdf = new MazUploadClass(); //creating new object class
 $test= $_FILES['pdf']; //getting file information from html page
 $siz = 2097152; //size in Byte (2MB)
 $fletype = "pdf"; //File type that you want to allow user to upload, currently supported formats are, image, pdf, audio, video, 
-/*-------------------------------------------------------------
-* @ return $errorFlag
-* @validate the input type
-*-------------------------------------------------------------*/
+/**
+* @return string $errorFlag
+* validate the input type
+*/
 $uploadpdf->uploadPDF($test,  $siz, $fletype); 
 /*----------/end of validation--------*/
 
@@ -24,31 +25,32 @@ if($uploadpdf->errorFlag['fileFlag'] == "yes"){
 	echo "yeah..got the class working";
 
 
-/*--------------------------------------------------------------
+/**
 * @Directory to save file: you can specify your own directory or you can call existing class function
 * @upload file: you can use either class function or your own
-*------------------------------------------------------------------------*/
+*/
 $uploadToFolder = "image"; //Folder name you want to upload your file under...yourroot/user_uploads, you can use this variable with createpath() function
 
-/*------------------------------------
+/**
 * @return a path ($dir) dynamically created every time when you upload something
 * the path is ... user_uploads/$uploadToFolder/currentyear/currentmonth/currentdate/
-* @if you want your own upload path don't call this function, instead use your own path and add your path with original path below
-* @it returns path
-*--------------------------------------*/
+* if you want your own upload path don't call this function, instead use your own path and add your path with original path below
+*/
 	$uploadpdf->createPath($fletype);  //you can replace the variable :)
-/*------------------
+/**
 * random code
-*-------------------------------*/
+*/
 	$uploadpdf->randomCode();
 
 	$tmpFileName = $_FILES['pdf']['tmp_name'];
-/*----------------
-*@if you use custom path make sure your path is already created in the server
+/**
+* if you use custom path make sure your path is already created in the server
 * and include your path in $originalPath instead of using this '$uploadpdf->path'
-*-----------------------------------*/
-	$originalPath = $uploadpdf->path.$uploadpdf->randomDigit.$test['name'];  //You can save this path into database as string 
-
+*/
+	$originalPath = $uploadpdf->path.$uploadpdf->randomDigit.$test['name'];  
+	/**
+	 * You can save this path into database as string 
+     */
 	$uploadpdf->uploadToServer($tmpFileName, $originalPath);
 
 }
